0f7f16de38420f0b20f21bc4b9cfcf7ae843d391
DennisTjeerdsma <dennistjeerdsma@hotmail.com>
2018-09-29 13:17:57 +0200

    initial

---
+++ app/main/forms.py
@@ -0,0 +1,84 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, SelectField
+from wtforms.validators import ValidationError, DataRequired, Length, Optional
+from app.models import User
+from datetime import datetime
+
+
+class RequiredIf(DataRequired):
+    """Validator which makes a field required if another field is set and has a truthy value.
+
+    Sources:
+        - http://wtforms.simplecodes.com/docs/1.0.1/validators.html
+        - http://stackoverflow.com/questions/8463209/how-to-make-a-field-conditionally-optional-in-wtforms
+        - https://gist.github.com/devxoul/7638142#file-wtf_required_if-py
+    """
+    field_flags = ('requiredif',)
+
+    def __init__(self, message=None, *args, **kwargs):
+        super(RequiredIf).__init__()
+        self.message = message
+        self.conditions = kwargs
+
+    """field is requiring that name field in the form is data value in the form"""
+    def __call__(self, form, field):
+        for name, data in self.conditions.items():
+            other_field = form[name]
+            if other_field is None:
+                raise Exception('no field named "%s" in form' % name)
+            if other_field.data == data and not field.data:
+                DataRequired.__call__(self, form, field)
+            Optional()(form, field)
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me',
+                             validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username.')
+
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    post = TextAreaField('Say something', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class QuoteForm(FlaskForm):
+    quote = TextAreaField('Enter a Quote', validators=[DataRequired(), Length(min=1, max=140)])
+    name = StringField('Name', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class EventForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired(), Length(min=1,max=140)])
+    post = TextAreaField('Description', validators=[Length(min=0, max=500)])
+    location = StringField('Location', validators=[Length(min=0)])
+    dropdown = SelectField(choices=[("No", "No"), ("Yes", "Yes")], id="dropdown", validators=[DataRequired()])
+    start_time = StringField('Event start', id='datetimepicker1', validators=[DataRequired()])
+    end_time = StringField('Event end', id='datetimepicker2', validators=[DataRequired()])
+    ev_time = StringField('Enrollment closed', id='datetimepicker3', validators=[RequiredIf(dropdown="Yes")])
+    submit = SubmitField('Submit')
+
+    def validate_end_time(self, args):
+        if self.end_time.data is None or self.start_time is None or self.ev_time is None:
+            return
+        if datetime.strptime(self.end_time.data, '%d/%m/%Y %H:%M') < datetime.strptime(self.start_time.data, '%d/%m/%Y %H:%M'):
+            raise ValidationError('End time cannot be before start time')
+    
+    def validate_ev_time(self, args):
+        if self.end_time.data is None or self.start_time is None or self.ev_time is None:
+            return
+        if datetime.strptime(self.ev_time.data, '%d/%m/%Y %H:%M') > datetime.strptime(self.start_time.data, '%d/%m/%Y %H:%M'):
+            raise ValidationError('Enrollment cannot close after event has started')
+
---
+++ app/main/views.py
@@ -0,0 +1,73 @@
+from flask_admin.contrib.sqla import ModelView
+from app.models import User, ACCESS, Event
+from wtforms import TextField
+from werkzeug.security import generate_password_hash
+from flask_login import current_user
+
+
+class MyPassField(TextField):
+    def process_data(self, value):
+        self.data = ''  # even if password is already set, don't show hash here
+        self.orig_hash = value
+
+    def process_formdata(self, valuelist):
+        value = ''
+        if valuelist:
+            value = valuelist[0]
+        if value:
+            self.data = generate_password_hash(value)
+        else:
+            self.data = self.orig_hash
+
+
+class UserView(ModelView):
+    form_overrides = dict(
+        password_hash=MyPassField,
+    )
+    form_widget_args = dict(
+        password_hash=dict(
+            placeholder='Enter new password here to change password',
+        ),
+    )
+
+    def is_accessible(self):
+        if current_user.is_authenticated:
+            user = User.query.filter_by(username=current_user.username).first()
+            return ACCESS[user.access] >= ACCESS["bestuur"]
+        else:
+            return current_user.is_authenticated
+
+    column_exclude_list = ("password_hash", "about_me")
+    column_labels = dict(bday="Birthday", access="Access Level", password_hash="Password")
+    form_excluded_columns = ["posts", "last_seen", "quotes", "followers", "followed", "participants"]
+
+    form_choices = {
+        "access": [
+            ("guest", "guest"),
+            ("user", "user"),
+            ("vakblad", "vakblad"),
+            ("bestuur", "bestuur"),
+            ("admin", "admin")
+        ]
+    }
+
+class EventView(ModelView):
+
+    def is_accessible(access_level):
+        if current_user.is_authenticated:
+            user = User.query.filter_by(username=current_user.username).first()
+            return ACCESS[user.access] >= ACCESS["bestuur"]
+        else:
+            return current_user.is_authenticated
+
+    column_list = ("title", "location", "enroll", "start_time", "end_time", "ev_time")
+    column_labels = dict(title="Name", enroll="Enroll [Yes/No]", ev_time="Enrollment closes", body="Description")
+    form_columns = ["title", "location", "body", "enroll", "start_time", "end_time",
+                    "ev_time", "participants"]
+
+    form_choices = {
+        "enroll": [
+            ("No", "No"),
+            ("Yes", "Yes")
+        ]
+    }
