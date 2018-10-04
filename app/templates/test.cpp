17c00337b65e8139e45ca45ec225c4a7db5ab54a
DennisTjeerdsma <dennistjeerdsma@hotmail.com>
2018-10-04 16:24:08 +0200

    last

---
+++ app/templates/test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main (){
+    int a
+    int b 
+    int sum
+
+    cout << "Give me a number \n";
+    cin >> a;
+
+    cout << "Give me another number";
+    cin >> b;
+
+    sum = a + b;
+    cout << "The sum is " << sum << endl; 
+};
